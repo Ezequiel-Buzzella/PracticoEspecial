34af1dd16f1bb99900bca0acdba0f02a05d6a388
Ezequiel-Buzzella <ezequiel.buzzella@gmail.com>
2025-09-02 14:45:01 -0300

    Creado carpeta Entity

---
+++ Ejercicio1/src/main/java/org/example/entity/Cliente.java
@@ -0,0 +1,14 @@
+package org.example.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Getter
+@Setter
+@ToString
+public class Cliente {
+}
---
+++ Ejercicio1/src/main/java/org/example/entity/Producto.java
@@ -0,0 +1,12 @@
+package org.example.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Producto {
+}
