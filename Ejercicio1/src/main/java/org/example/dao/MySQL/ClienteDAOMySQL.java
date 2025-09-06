5e4961bd7b6a6d771c092c5c53ca5b9e28ac7e33
Ezequiel-Buzzella <ezequiel.buzzella@gmail.com>
2025-09-06 13:02:23 -0300

    Hecho Dao de cliente

--- Ejercicio1/src/main/java/org/example/dao/MySQL/ClienteDAOMySQL.java
+++ Ejercicio1/src/main/java/org/example/dao/MySQL/ClienteDAOMySQL.java
@@ -1,28 +1,119 @@
 package org.example.dao.MySQL;
 
+import com.mysql.cj.xdevapi.PreparableStatement;
 import org.example.dao.interfaces.ClienteDAO;
 import org.example.entity.Cliente;
+import org.xml.sax.SAXException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDAOMySQL implements ClienteDAO {
+
+    private Connection conexion;
+
+
+    public ClienteDAOMySQL(Connection conexion) {
+        this.conexion = conexion;
+    }
     @Override
-    public List<Cliente> listarTodo() {
-        return List.of();
+    public List<Cliente> listarTodo() throws SQLException{
+        List<Cliente> lista = new ArrayList<>();
+        String query = "SELECT * FROM Cliente";
+         try {
+            PreparedStatement ps = this.conexion.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Cliente cliente = new Cliente(
+                        rs.getInt("idCliente"),
+                        rs.getString("nombre"),
+                        rs.getString("email")
+                );
+                lista.add(cliente);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
     }
 
     @Override
-    public Cliente listarPorId(Integer id) {
+    public Cliente listarPorId(Integer id) throws SQLException{
+        String query = "SELECT * FROM Cliente WHREE idCliente=?";
+        Cliente cliente = null;
+        try(PreparedStatement ps = this.conexion.prepareStatement(query);){
+            ps.setInt(1, id);
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()){
+                    int idCliente = rs.getInt("idCliente");
+                    String nombre =  rs.getString("nombre");
+                    String email = rs.getString("email");
+                    Cliente c = new Cliente(idCliente, nombre, email);
+                }
+            }catch(SQLException e){
+                throw new RuntimeException(e);
+            }
+
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
-    public void agregar(Cliente cliente) {
+    public void agregar(Cliente cliente) throws SQLException {
+        String query =  "INSERT INTO Cliente (idCliente,nombre, email) VALUES (?,?, ?)";
+        try(PreparedStatement ps = this.conexion.prepareStatement(query);){
+            ps.setInt(1, cliente.getIdCliente());
+            ps.setString(2, cliente.getNombre());
+            ps.setString(3, cliente.getEmail());
+            ps.executeUpdate();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     @Override
     public void eliminar(Cliente cliente) {
+        String query = "DELETE FROM Cliente WHERE idCliente=?";
+        try(PreparedStatement ps = this.conexion.prepareStatement(query);){
+            ps.setInt(1, cliente.getIdCliente());
+            int rowsaffected = ps.executeUpdate();
 
+            if(rowsaffected == 0){
+                throw new RuntimeException("No se pudo eliminar el cliente");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+
+    @Override
+    public List<Cliente> getClientesCOnMayorFacturacion() throws SQLException {
+
+        String query = "SELECT nombre, SUM(fp.cantidad * p.valor) as totalFacturado FROM Cliente c" +
+                "JOIN Factura f ON f.idCliente=c.idCliente" +
+                "JOIN Factura_Producto ON fp.idFactura=f.idFactura" +
+                "JOIN Producto p ON p.idProducto=f.idProducto" +
+                "GROUP BY c.nombre" +
+                "ORDER BY totalFacturado DESC";
+        List<Cliente> clientes = new ArrayList<>();
+        try(PreparedStatement ps = this.conexion.prepareStatement(query);
+            ResultSet rs = ps.executeQuery()){
+                while (rs.next()) {
+                    clientes.add(new Cliente(
+                            rs.getString("nombre"),
+                            rs.getFloat("totalFacturado")
+                    ));
+                }
+            }
+        return clientes;
+
+        }
+    }
--- Ejercicio1/src/main/java/org/example/dao/interfaces/ClienteDAO.java
+++ Ejercicio1/src/main/java/org/example/dao/interfaces/ClienteDAO.java
@@ -2,6 +2,8 @@ package org.example.dao.interfaces;
 
 import org.example.entity.Cliente;
 
+import java.util.List;
+
 public interface ClienteDAO extends DAO<Cliente> {
-    
+    public List<Cliente> getClientesCOnMayorFacturacion() throws  Exception;
 }
