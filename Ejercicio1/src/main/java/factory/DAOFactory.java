dc36f491ba7ab1928ab6290d70279818cf3fb8f2
Francisco Vazquez <franvazquezcarrasco@gmail.com>
2025-09-10 13:56:53 -0300

    Implementación de DAOFactory para MySQL

--- Ejercicio1/src/main/java/factory/DAOFactory.java
+++ Ejercicio1/src/main/java/factory/DAOFactory.java
@@ -1,21 +1,23 @@
 package factory;
 
 
+import org.example.dao.interfaces.DAO;
+
+import java.sql.SQLException;
+
 public abstract class DAOFactory {
 
-
-    public static DAOFactory getDAOFactory(int database) {
+    public static DAOFactory getDAOFactory(int database) throws ClassNotFoundException {
         switch(database){
             case 1:
-                return new MysqlDAOFactory();
+                return MysqlDAOFactory.getInstance();
             default:
                 throw new IllegalArgumentException("Fábrica no soportada: " + database);
         }
     }
 
-    /*public abstract ProductoDAO getProductoDAO();
-    public abstract ClienteDAO getClienteDAO();
-    public abstract FacturaDAO getFacturaDAO();
-    public abstract Factura_ProductoDAO getFactura_ProductoDAO();*/
-
+    public abstract DAO getProductoDAO() throws SQLException;
+    public abstract DAO getClienteDAO() throws SQLException;
+    public abstract DAO getFacturaDAO() throws SQLException;
+    public abstract DAO getFactura_ProductoDAO() throws SQLException;
 }
--- Ejercicio1/src/main/java/factory/MysqlDAOFactory.java
+++ Ejercicio1/src/main/java/factory/MysqlDAOFactory.java
@@ -1,5 +1,11 @@
 package factory;
 
+import org.example.dao.MySQL.ClienteDAOMySQL;
+import org.example.dao.MySQL.FacturaDAOMySQL;
+import org.example.dao.MySQL.Factura_ProductoDAOMySQL;
+import org.example.dao.MySQL.ProductoDAOMySQL;
+import org.example.dao.interfaces.DAO;
+
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,15 +13,29 @@ import java.sql.SQLException;
 
 public class MysqlDAOFactory extends DAOFactory {
 
-    String driver =  "com.mysql.cj.jdbc.Driver";
-    String url = "jdbc:mysql://localhost:3307/Entregable1";
-    String password = "root";
-    String user = "root";
+    private static MysqlDAOFactory instance;
 
+    private Connection conn;
+
+    private final String DRIVER =  "com.mysql.cj.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3307/Entregable1";
+    private final String PASSWORD = "root";
+    private final String USER = "root";
+
+    public static MysqlDAOFactory getInstance() throws ClassNotFoundException {
+        if(instance == null) {
+            instance = new MysqlDAOFactory();
+        }
+        return instance;
+    }
+
+    private MysqlDAOFactory() throws ClassNotFoundException {
+        this.conn = getConnection();
+    }
 
     public Connection getConnection() throws ClassNotFoundException{
         try {
-            Class.forName(this.driver).getDeclaredConstructor().newInstance();
+            Class.forName(this.DRIVER).getDeclaredConstructor().newInstance();
         } catch (InstantiationException e) {
             throw new RuntimeException(e);
         } catch (IllegalAccessException e) {
@@ -27,11 +47,29 @@ public class MysqlDAOFactory extends DAOFactory {
         }
         Connection conexion;
         try {
-             conexion = DriverManager.getConnection(url,user,password);
+             conexion = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return conexion;
     }
 
+    @Override
+    public DAO getClienteDAO() throws SQLException {
+        return ClienteDAOMySQL.obtenerInstancia(this.conn);
+    }
+    @Override
+    public DAO getProductoDAO() throws SQLException {
+        return ProductoDAOMySQL.obtenerInstancia(this.conn);
+    }
+
+    @Override
+    public DAO getFacturaDAO() throws SQLException {
+        return FacturaDAOMySQL.obtenerInstancia(this.conn);
+    }
+
+    @Override
+    public DAO getFactura_ProductoDAO() throws SQLException {
+        return Factura_ProductoDAOMySQL.obtenerInstancia(this.conn);
+    }
 }
