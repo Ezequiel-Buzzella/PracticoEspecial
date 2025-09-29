82d506349b8f54fc1aa9eb490d1eb18ba17e6497
Ezequiel-Buzzella <ezequiel.buzzella@gmail.com>
2025-09-29 17:04:43 -0300

    Corrección error git

--- Ejercicio1/src/main/java/org/example/dao/interfaces/ProductoDAO.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao.interfaces;
-
-import org.example.entity.Producto;
-
-import java.util.List;
-
-public interface ProductoDAO extends DAO<Producto, Integer> {
-    Producto productoQueMasRecaudo() throws Exception;
-}
